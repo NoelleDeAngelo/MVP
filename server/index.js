f1b02ba2fcf2b456595f5ef43c0e8a04f5b80526
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-05-18 14:06:37 -0700

    Add participants video feed to the page

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const {v4: uuidV4} = require('uuid');
+
+app.use(express.static(__dirname + '/../client/dist'))
+app.set('view engine', 'ejs');
+app.set('views', __dirname+ '/../client/dist')
+
+app.get('/', (req, res)=>{
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res)=>{
+  res.render('index.ejs', {roomId: req.params.room})
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.broadcast.emit('user-connected', userId)
+  } )
+})
+
+
+server.listen(3000)
